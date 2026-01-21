8b1e322458553798e53a2108bc6d6e7af47c05f1
Raffi Molero <raffimolero@gmail.com>
2026-01-22 00:38:05 +0800

    v1.6

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,8 @@
 
 mod prelude {
     pub use crate::{
-        disharmonizer_stack::disharmonizer_stack,
-        gold_factory::gold_factory,
-        pure_factory::pure_factory,
-        spark_factory::spark_factory,
-        storage::{all_items, storage, trash},
-        util::{chain_ports, split_inputs_outputs, stack, stack_vec},
+        disharmonizer_stack::disharmonizer_stack, gold_factory::gold_factory,
+        pure_factory::pure_factory, spark_factory::spark_factory, storage::*, util::*,
     };
 
     pub use immortality_factory_laboratory::prelude::*;
@@ -25,12 +21,34 @@ mod util;
 use crate::{prelude::*, util::export};
 
 fn stuff() -> World {
+    // let mut bp = World::new();
+    // let pump = bp.place(AirPump, 0, 0);
+    // let refine = bp.place(Refinery, 2, 0);
+    // bp.connect(pump.output(0), refine.input(0));
+    // let bp = Blueprint {
+    //     contents: bp,
+    //     size: Size {
+    //         w: AirPump.width() + Refinery.width(),
+    //         h: 2,
+    //     },
+    //     inputs: vec![],
+    //     outputs: vec![refine.output(0)],
+    // };
+    // let mut world = World::new();
+    // let mc0 = world.place(&bp, 0, 0);
+    // let mc1 = world.place(&bp, 0, 2);
+    // let merge = world.place(Merger, 0, 4);
+    // world.connect(mc0.output(0), merge.input(0));
+    // world.connect(mc1.output(0), merge.input(1));
+    // world
+
+    // World::new()
     // gold_factory::gold_demo()
     // spark_factory::spark_demo()
     // pure_factory::pure_demo()
     // spark_factory::pure_spark_demo()
-    chassis_factory::chassis_demo()
-    // chassis_factory::pure_chassis_demo()
+    // chassis_factory::chassis_demo()
+    chassis_factory::pure_chassis_demo()
     // stress_test::ideal()
     // stress_test::buildings()
     // stress_test::pumps()
@@ -49,7 +67,7 @@ fn get_pump_count(world: &World) -> usize {
 fn main() {
     let mut world = World::new();
     world.place(Laboratory, 0, -2);
-    // world.place(&all_items(16), -100, -100);
+    // world.place(&all_items(4), -100, -100);
     world.place(&stuff(), 0, 0);
 
     println!("{} Air Pumps.", get_pump_count(&world));
@@ -57,5 +75,6 @@ fn main() {
     let structure_size = size_of_val(&world.structures[0]);
     let structure_bytes = world.structures.len() * structure_size;
     println!("{structure_bytes} bytes from structures ({structure_size} bytes/struct",);
+    // dbg!(&world);
     export(&world, "../save_v1_6.ini").expect("Could not export world.");
 }
--- src/storage.rs
+++ src/storage.rs
@@ -24,6 +24,50 @@ pub fn trash(columns: usize) -> Blueprint {
     }
 }
 
+fn sell_one() -> Blueprint {
+    let mut bp = World::new();
+    let bs = bp.place(BigSplitter, 0, 0);
+    let bm = bp.place(BigMerger, 1, 0);
+    for j in 0..4 {
+        let ad = bp.place(
+            SubdimensionalMarket,
+            2 + j * SubdimensionalMarket.width(),
+            0,
+        );
+        bp.connect(bs.output(j as usize), ad.input(0));
+        bp.connect(ad.output(0), bm.input(j as usize));
+    }
+    Blueprint {
+        contents: bp,
+        size: Size {
+            w: 2 + SubdimensionalMarket.width() * 4,
+            h: BigSplitter.height(),
+        },
+        inputs: vec![bm.input(4), bs.input(0)],
+        outputs: vec![bm.output(0), bs.output(4)],
+    }
+}
+
+/// inputs: [gold, <any>]
+///
+/// outputs: [gold, <any>]
+pub fn sell(rows: usize) -> Blueprint {
+    let mut bp = World::new();
+    let row = &sell_one();
+    let sps = stack_vec(rows, |i| bp.place(row, 0, i * row.height()));
+    let (inputs, outputs) = split_inputs_outputs(chain_ports(&mut bp, &sps, [(0, 0), (1, 1)]));
+    Blueprint {
+        contents: bp,
+        size: row.size()
+            * Size {
+                w: 1,
+                h: rows as Coord,
+            },
+        inputs,
+        outputs,
+    }
+}
+
 pub fn storage(count: usize, rows: usize, item: Item) -> Blueprint {
     let rows = rows as Coord;
     let mut bp = World::new();
@@ -48,6 +92,28 @@ pub fn storage(count: usize, rows: usize, item: Item) -> Blueprint {
         },
         inputs,
         outputs,
+    }
+}
+
+/// literally only pipes the input into the first input of primary
+/// and only gives you the first output of primary
+///
+/// if you want customizability, go place them manually
+pub fn overflow_buffer(trash_columns: usize, primary_bp: &Blueprint) -> Blueprint {
+    let mut bp = World::new();
+    let overflow_bp = &trash(trash_columns);
+    let overflow = bp.place(overflow_bp, 0, 0);
+    let primary = bp.place(primary_bp, overflow.width(), 0);
+    bp.connect(overflow.output(0), primary.input(0));
+    Blueprint {
+        contents: bp,
+        size: Size {
+            w: overflow_bp.width() + primary_bp.width(),
+            h: overflow_bp.height().max(primary_bp.height()),
+        },
+        inputs: vec![overflow.input(0)],
+        // outputs are instead accessed through primary and overflow
+        outputs: vec![primary.output(0)],
     }
 }
 
